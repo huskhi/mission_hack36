f4647967fedd4b37f048209a2d847f7575a723a0
Dikshika <b20194@students.iitmandi.ac.in>
2022-02-27 22:02:43 +0530

    ..

--- app.js
+++ app.js
@@ -57,6 +57,6 @@ app.use(function(req, res, next) {
 app.use('/', require('./routes/index.js'));
 app.use('/users', require('./routes/users.js'));
 
-const PORT = process.env.PORT || 7000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log(`Server running on  ${PORT}`));
